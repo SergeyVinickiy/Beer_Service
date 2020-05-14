779d1aa1aac0ad370f8d3f7018d04b5c3db19fdf
SergeyV1 <sergey.vinickiy@verifone.com>
2020-05-14 21:33:11 +0300

    adding beer dto validation

--- src/main/java/com/vinickiy/beerservice/web/model/BeerDto.java
+++ src/main/java/com/vinickiy/beerservice/web/model/BeerDto.java
@@ -4,7 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -16,13 +19,30 @@ import java.util.UUID;
 @Builder
 public class BeerDto  {
 
+    @Null
     private UUID id;
+
+    @Null
     private Integer version;
+
+    @Null
     private OffsetDateTime createdDate;
+
+    @Null
     private OffsetDateTime lastModifiedeDate;
+
+    @NotBlank
     private String beerName;
+
+    @NotNull
     private BeerStyleEnum beerStyle;
+
+    @NotNull
+    @Positive
     private Long upc;
+
+    @NotNull
+    @Positive
     private BigDecimal price;
     private Integer quantityOnHand;
 
